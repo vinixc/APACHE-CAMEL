7741551afb7b5151dc382ea66e82101820a12bf8
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-20 22:48:15 -0300

    XStreamDataFormat e split

--- src/main/java/br/com/vini/camel/RotaPedidos2HttpPolling.java
+++ src/main/java/br/com/vini/camel/RotaPedidos2HttpPolling.java
@@ -1,9 +1,13 @@
 package br.com.vini.camel;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.vini.camel.model.Negociacao;
 
 public class RotaPedidos2HttpPolling {
 	//http://localhost:8080/webservices/ebook/item
@@ -11,17 +15,23 @@ public class RotaPedidos2HttpPolling {
 
 		CamelContext context = new DefaultCamelContext();
 		
+		
 		context.addRoutes(new RouteBuilder() {
 
 			@Override
 			public void configure() throws Exception {
+				final XStream xstream = new XStream();
+				xstream.alias("negociacao", Negociacao.class);
 				
 				from("timer://negociacoes?fixedRate=true&delay=1s&period=360s")
 				.to("http4://argentumws-spring.herokuapp.com/negociacoes")
 				.convertBodyTo(String.class)
+				.unmarshal(new XStreamDataFormat(xstream))
+				.split(body())
 				.log("${body}")
-				.setHeader(Exchange.FILE_NAME, constant("negociacoes.xml"))
-				.to("file:saida");
+//				.setHeader(Exchange.FILE_NAME, constant("negociacoes.xml"))
+//				.to("file:saida");
+				.end();
 				
 			}
 		});
